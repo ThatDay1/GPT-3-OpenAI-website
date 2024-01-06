877e4c80266d1a7d602569d8a8c4698226a29385
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2024-01-07 01:29:40 +0300

    footer and blog sections

--- gpt3_jsm/src/components/cta/Cta.jsx
+++ gpt3_jsm/src/components/cta/Cta.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './cta.css'
 
-const cta = () => {
-  return <div>cta</div>
+const CTA = () => {
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Gey Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  )
 }
 
-export default cta
+export default CTA
--- gpt3_jsm/src/containers/footer/Footer.jsx
+++ gpt3_jsm/src/containers/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Istanbul/Esenler, Gencosman, Ensar Cd. No:2</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Overons</p>
+          <p>Istanbul/Esenler, Gencosman, Ensar Cd. No:2</p>
+          <p>090-12345678</p>
+          <p>ogunkaraoglu62@gmail.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2024 GPT-3, All rights reserved.</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
