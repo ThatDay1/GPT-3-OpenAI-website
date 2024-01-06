3858044ee851cf10b9fd1dcff496a2f56ec5b42e
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2024-01-06 15:34:57 +0300

    navbar, header, brand and whatgpt3 components

--- gpt3_jsm/src/containers/index.jsx
+++ gpt3_jsm/src/containers/index.jsx
@@ -1,5 +1,5 @@
 export { default as Blog } from './blog/Blog.jsx'
-export { default as Features } from './features/Features.jsx'
+export { default as Features } from './features/Feature.jsx'
 export { default as Footer } from './footer/Footer.jsx'
 export { default as Header } from './header/Header.jsx'
 export { default as Possibility } from './possibility/Possibility.jsx'
